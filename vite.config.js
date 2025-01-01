4a7c97cfc3eb3ee7f3a0d258f02a464f43ee61ba
yousseforabi <122828573+yousseforabi@users.noreply.github.com>
2025-01-01 22:57:49 +0100

    FIXING VITE

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://yousseforabi.github.io/portfolio/',   // Ensure correct base path for GitHub Pages
+  build: {
+    outDir: 'dist',  
 })
