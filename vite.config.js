1340259a718218a79be64b9f088ae1e1be59137f
yousseforabi <122828573+yousseforabi@users.noreply.github.com>
2025-01-02 00:46:02 +0100

    Save current work

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/portfolio/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
